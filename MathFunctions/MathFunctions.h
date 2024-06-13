32df64682bd3dcd121f1e2ece863cf1e9fa40821
John <iplanetcn@gmail.com>
2024-06-13 11:52:35 +0800

    update project name

--- MathFunctions/MathFunctions.h
+++ MathFunctions/MathFunctions.h
@@ -1,14 +1,7 @@
-//
-// Created by John Lee on 9/16/21.
-//
-
-#ifndef TUTORIAL_MATHFUNCTIONS_H
-#define TUTORIAL_MATHFUNCTIONS_H
+#pragma once
 
 #include <cstdio>
 #include <cmath>
 #include <cstdlib>
 
 double mysqrt(double a);
-
-#endif //TUTORIAL_MATHFUNCTIONS_H
